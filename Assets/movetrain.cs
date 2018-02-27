f758b10c811ea8b8ebce19b5c0187fb7f46e81ee
Jenny Xia <33613456+xiajenny@users.noreply.github.com>
2018-02-27 11:34:49 -0500

    Files

---
+++ Assets/movetrain.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movetrain : MonoBehaviour {
+	
+	Vector3 position1 = new Vector3(-158.4f, 1.373373e-17f, -29.24922f);
+	Vector3 position2 = new Vector3(200f, 1.373373e-17f, -29.24922f);
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = Vector3.MoveTowards (transform.position, position2, 15 * Time.deltaTime);
+
+		if (transform.position.x == 200f) {
+
+			transform.position = position1;
+
+			transform.position = Vector3.MoveTowards (transform.position, position2, 23 * Time.deltaTime);
+	}
+}
+
+}
---
+++ Assets/train2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class train2 : MonoBehaviour
+{
+
+    Vector3 position1 = new Vector3(1.5f, 0f, 0f);
+    Vector3 position2 = new Vector3(-1f, 0.0f, 0.0f);
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, position2, 5 * Time.deltaTime);
+
+        if (transform.position.x == -1.0f)
+        {
+
+            transform.position = position1;
+
+            transform.position = Vector3.MoveTowards(transform.position, position2, 5 * Time.deltaTime);
+        }
+    }
+
+}
