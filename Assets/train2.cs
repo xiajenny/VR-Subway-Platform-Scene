d891581b372100a14177811fa4bb8073fac70458
Jenny Xia <33613456+xiajenny@users.noreply.github.com>
2018-03-05 11:04:13 -0500

    audio

--- Assets/train2.cs
+++ Assets/train2.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class train2 : MonoBehaviour
 {
-
+    public AudioClip slowsubway;
+    AudioSource audio;
     Vector3 position1 = new Vector3(130f, 0f, 0f);
     Vector3 position2 = new Vector3(-20f, 0.0f, 0.0f);
+
 
     // Use this for initialization
     void Start()
     {
-
+        audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,11 +23,13 @@ public class train2 : MonoBehaviour
 
         transform.position = Vector3.MoveTowards(transform.position, position2, 5 * Time.deltaTime);
 
-        if (transform.position.x == -20f)
+            if (transform.position.x == -20f)
         {
+            audio.PlayOneShot(slowsubway, 1.0f);
+            audio.Play();
+            Debug.Log("play audio");
 
             transform.position = position1;
-
             transform.position = Vector3.MoveTowards(transform.position, position2, 5 * Time.deltaTime);
         }
     }
