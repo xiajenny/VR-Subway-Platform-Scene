d76f49cdae527c47bba94ef0b5f0dc50af19ee92
Jenny Xia <33613456+xiajenny@users.noreply.github.com>
2018-03-08 15:41:48 -0500

    updated train sequencing

--- Assets/train2.cs
+++ Assets/train2.cs
@@ -7,9 +7,11 @@ public class train2 : MonoBehaviour
 {
     public AudioClip slowsubway;
     AudioSource audio;
+    public AudioClip chime;
+    AudioSource audiochime;
     Vector3 position1 = new Vector3(160f, 0f, 0f);
     Vector3 position2 = new Vector3(20f, 0.0f, 0.0f);
-    Vector3 position3 = new Vector3(-80f, 0.0f, 0.0f);
+    Vector3 position3 = new Vector3(-140f, 0.0f, 0.0f);
     int count = 0;
     bool play;
 
@@ -17,6 +19,7 @@ public class train2 : MonoBehaviour
     void Start()
     {
         audio = GetComponent<AudioSource>();
+        audiochime = GetComponent<AudioSource>();
         audio.PlayOneShot(slowsubway, 1.0f);
 
     }
@@ -29,23 +32,38 @@ public class train2 : MonoBehaviour
             transform.position = Vector3.MoveTowards(transform.position, position2, 5 * Time.deltaTime);
         }
 
-        if (transform.position.x == 20f)
+        if (count == 500)
          {
+            audio.PlayOneShot(chime, 1.0f);
             count = count + 1;
             Debug.Log("Count" + count);
          }
 
-        if (count >= 500)
+        if (transform.position.x == 20f)
+        {
+            count = count + 1;
+            Debug.Log("Count" + count);
+        }
+
+        if (count >= 700 && play == false)
+        {
+            audio.PlayOneShot(slowsubway, 1.0f);
+            play = true;
+            Debug.Log("play audio");
+            transform.position = Vector3.MoveTowards(transform.position, position3, 5 * Time.deltaTime);
+        }
+
+        if (count >= 700)
         {
             transform.position = Vector3.MoveTowards(transform.position, position3, 5 * Time.deltaTime);
         }
 
-        if (transform.position.x == -80f && count != 0)
+        if (transform.position.x == -140f && count != 0)
         {
             count = 0;
             transform.position = position1;
             audio.PlayOneShot(slowsubway, 1.0f);
-            play = true;
+            play = false;
             Debug.Log("play audio");
         }
 
